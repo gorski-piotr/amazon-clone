37d8000f8eb0da4d8bec7cbd197d1993f5c9b12c
Piotr Gorski <gorskipiotr5@gmail.com>
2021-05-20 21:37:33 +0100

    tailwind css and next.js (for images) configured. Header in progress

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,14 @@
 import Head from "next/head";
+import Header from "../components/Header";
 
 export default function Home() {
   return (
     <div>
       <Head>
-        <title>Amazon 2.0</title>
+        <title>PG Amazon Clone</title>
       </Head>
 
-      <h1>There will be an Amazon Clone</h1>
+      <Header />
     </div>
   );
 }
